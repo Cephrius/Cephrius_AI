28b1d563288084611d2b439085d663ef6dc36b62
Chiedozie Ehileme <ehilemece@hc.edu>
2024-07-15 00:46:29 -0500

    update README.md and script.js

--- open_ai_backend/script.js
+++ open_ai_backend/script.js
@@ -1,15 +1,15 @@
-import { config } from "dotenv";
-config();
+// import { config } from "dotenv";
+// config();
 
-import OpenAI from "openai";
-import readline from "readline";
+// import OpenAI from "openai";
+// import readline from "readline";
 
-const openAi = new OpenAI({
-  apiKey: process.env.OPEN_AI_API_KEY,
-});
+// const openAi = new OpenAI({
+//   apiKey: process.env.OPEN_AI_API_KEY,
+// });
 
-const response = await openAi.chat.completions.create({
-  model: "gpt-3.5-turbo",
-  messages: [{ role: "user", content: "Hello chatGPT" }],
-});
-console.log(response.choices[0].message);
+// const response = await openAi.chat.completions.create({
+//   model: "gpt-3.5-turbo",
+//   messages: [{ role: "user", content: "Hello chatGPT" }],
+// });
+// console.log(response.choices[0].message);
